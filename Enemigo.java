1d12f12a347da6dbda9932a1a186c3da7770343a
Omarx06 <lopez.omar60@uabc.edu.mx>
2025-11-26 12:36:11 -0800

    Delete Enemigo.java

--- Enemigo.java
+++
@@ -1,9 +0,0 @@
-package com.example.juegojavafx.juegojavafx;
-
-public abstract class Enemigo extends Entidad {
-    protected double velX = 0;
-    public Enemigo(double x, double y, double width, double height, double velX) {
-        super(x,y,width,height);
-        this.velX = velX;
-    }
-}
