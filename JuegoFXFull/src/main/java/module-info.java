1c3a6d841f06600a0dfc055c26b42074befa99ce
Omarx06 <lopez.omar60@uabc.edu.mx>
2025-11-26 15:36:30 -0800

    Delete JuegoFXFull directory

--- JuegoFXFull/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.juegofxfull.juegofxfull {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.juegofxfull.juegofxfull to javafx.fxml;
-    exports com.example.juegofxfull.juegofxfull;
-}
